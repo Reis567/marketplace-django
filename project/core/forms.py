1fde21562f9ca3936eda603f9c8db1c801123362
Reis567 <matheusdosreis9@gmail.com>
2023-07-20 06:53:39 -0300

    Sign up feature

--- project/core/forms.py
+++ project/core/forms.py
@@ -8,3 +8,21 @@ class SignupForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username','email','password1','password2' )
+
+    username = forms.CharField(widget=forms.TextInput(attrs={
+        'placeholder':'Your username',
+        'class':'w-full py-4 px-5 rounded-xl'
+                }))
+    email = forms.CharField(widget=forms.EmailInput(attrs={
+        'placeholder':'Your email',
+        'class':'w-full py-4 px-5 rounded-xl'
+                }))
+    password1 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Your password',
+        'class':'w-full py-4 px-5 rounded-xl'
+                }))
+    
+    password2 = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder':'Repeat your password',
+        'class':'w-full py-4 px-5 rounded-xl'
+                }))
